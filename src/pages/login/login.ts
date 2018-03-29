398637f3e6f56529d352936a6911c299a1c026ac
foo <bar@example.com>
2018-03-29 14:46:14 +0800

    Fixed Register Page.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -39,7 +39,7 @@ export class LoginPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
-    this.navCtrl.push(HomeDinerPage);
+    this.navCtrl.push(RegisterPage);
   }
 }
 
