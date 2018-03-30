fc856d3ef6345833fa445d2707eb60c822feb9e8
Relyn Dela Cruz <rbdc13@gmail.com>
2018-03-30 15:04:34 +0800

    register users

---
+++ src/models/registerdiner.interface.ts
@@ -0,0 +1,11 @@
+export interface RegisterDiner {
+	dine_name: string;
+	dine_owner_name: string;
+	dine_username: string;
+	dine_email: string;
+	dine_weblink: string;
+	dine_number: string;
+	dine_address: string;
+	dine_password: string;
+	dine_retypePassword: string;
+}
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,6 +1,12 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { RegisterCustomer } from '../../models/registercustomer.interface';
+import { RegisterDiner } from '../../models/registerdiner.interface'
+
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
 /**
  * Generated class for the RegisterPage page.
  *
@@ -24,16 +30,64 @@ export class RegisterPage {
 
 	// Diner form variables
 	@ViewChild('dine_name') dine_name;
+	@ViewChild('dine_owner_name') dine_owner_name;
 	@ViewChild('dine_username') dine_username;
 	@ViewChild('dine_email') dine_email;
+	@ViewChild('dine_weblink') dine_weblink;
+	@ViewChild('dine_number') dine_number;
+	@ViewChild('dine_address') dine_address;
 	@ViewChild('dine_password') dine_password;
 	@ViewChild('dine_retypePassword') dine_retypePassword;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	registerCustomer = {} as RegisterCustomer;
+	registerDiner = {} as RegisterDiner;
+
+	registerCustomerRef$: AngularFireList<RegisterCustomer>
+	registerDinerRef$: AngularFireList<RegisterDiner>
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private database: AngularFireDatabase, private fire: AngularFireAuth) {
+  	this.registerCustomerRef$ = this.database.list('customers');
+  	this.registerDinerRef$ = this.database.list('diners');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
+  }
+
+  createCustomer(registerCustomer: RegisterCustomer) {
+  	// console.log(registerCustomer);
+  	this.fire.auth.createUserWithEmailAndPassword(this.cust_email.value, this.cust_password.value)
+
+  	.then((data) => {
+  		console.log("Data: ", data);
+  	})
+  	.catch(error => {
+  		console.log("Error: ", error);
+  	})
+
+
+  	this.registerCustomerRef$.push({
+  		cust_name: this.registerCustomer.cust_name,
+  		cust_username: this.registerCustomer.cust_username,
+  		cust_email: this.registerCustomer.cust_email,
+  		cust_password: this.registerCustomer.cust_password,
+  		cust_retypePassword: this.registerCustomer.cust_retypePassword
+  	})
+  }
+
+  createDiner(registerDiner: RegisterDiner){
+  	// console.log(registerDiner);
+  	this.registerDinerRef$.push({
+  		dine_name: this.registerDiner.dine_name,
+  		dine_owner_name: this.registerDiner.dine_owner_name,
+  		dine_username: this.registerDiner.dine_username,
+  		dine_email: this.registerDiner.dine_email,
+  		dine_weblink: this.registerDiner.dine_weblink,
+  		dine_number: this.registerDiner.dine_number,
+  		dine_address: this.registerDiner.dine_address,
+  		dine_password: this.registerDiner.dine_password,
+  		dine_retypePassword: this.registerDiner.dine_address
+  	})
   }
 
   closePage(){
