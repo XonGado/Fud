387445cbcc40ece85eb09cc77952c97480eb5e54
Relyn Dela Cruz <rbdc13@gmail.com>
2018-04-15 00:47:06 +0800

    update item

--- src/pages/item-edit/item-edit.ts
+++ src/pages/item-edit/item-edit.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { AngularFirestore, AngularFirestoreModule, AngularFirestoreCollection } from 'angularfire2/firestore';
-import { Observable } from 'rxjs/Observable'
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
 
 import { Item } from '../../models/item.model';
 
@@ -20,14 +21,52 @@ import { Item } from '../../models/item.model';
 })
 export class ItemEditPage {
 	item_id: string;
+	item_name: string;
+	item_type: string;
+	item_price: string;
+	item_description: string;
 
-	
-	constructor(public navCtrl: NavController, public navParams: NavParams) {
+	items: Observable<Item[]>
+	itemsCollectionRef: AngularFirestoreCollection<Item>
+
+	@ViewChild('name') name;
+	@ViewChild('type') type;
+	@ViewChild('price') price;
+	@ViewChild('description') description;
+
+	constructor(public navCtrl: NavController, public navParams: NavParams, private firestore: AngularFirestore, private fire: AngularFireAuth) {
 		this.item_id = navParams.get('data');
+		this.itemsCollectionRef = this.firestore.collection('diners').doc(this.fire.auth.currentUser.uid).collection('items')
 	}
 
 	ionViewDidLoad() {
+		this.fetchData(this.item_id)
 		console.log('ionViewDidLoad ItemEditPage');
+	}
+
+	fetchData(item_id){
+		let that = this
+		this.itemsCollectionRef.doc(item_id).ref.get()
+		.then(doc => {
+			that.item_id = doc.data().item_id
+			that.item_name = doc.data().item_name
+			that.item_type = doc.data().item_type
+			that.item_price = doc.data().item_price
+			that.item_description = doc.data().item_description
+		})
+		.catch(error => {
+			console.log("Error: ", error.code)
+		})
+	}
+
+	updateItem(){
+		this.itemsCollectionRef.doc(this.item_id).update({
+			item_id: this.item_id,
+			item_name: this.name.value,
+			item_type: this.type.value,
+			item_price: this.price.value,
+			item_description: this.description.value
+		})
 	}
 
 	closePage(){
